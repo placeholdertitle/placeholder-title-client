78f9cd6eaa32a9a3da91f171c4a99c086e42cec9
Michael Upton <uptonm@wit.edu>
2019-04-09 15:54:07 -0400

    Working on navbar auth logic

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        Hello World
-      </div>
+      <Navbar />
     )
   }
 }
---
+++ client/src/controllers/userService.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const getUser = async () => {
+  const user = await axios.get('https://randomuser.me/api/?results=1');
+  return user.data.results[0];
+}
+
+export default getUser;
