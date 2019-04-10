f2b12448ab4365dd4d72925f3482f82b68a26af2
Michael Upton <uptonm@wit.edu>
2019-04-10 10:58:54 -0400

    Prettier Refactor + More - Auth is now stored in the parent comp App - Auth methods (login/logout) are now stored in App - All auth logic will exist inside of App in the future

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Navbar from './Navbar';
+import getUser from '../controllers/userService';
 
 class App extends Component {
+  state = {
+    user: false
+  };
+  componentDidMount = () => {
+    this.setState({ user: false });
+  };
+  logout = () => {
+    this.setState({ user: false });
+  };
+  login = async () => {
+    const user = await getUser();
+    this.setState({ user });
+  };
   render() {
     return (
       <div>
         <BrowserRouter>
-          <Navbar />
+          <Navbar
+            user={this.state.user}
+            login={this.login.bind(this)}
+            logout={this.logout.bind(this)}
+          />
           <Route />
         </BrowserRouter>
       </div>
-    )
+    );
   }
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './assets/scss/main.scss'
+import './assets/scss/main.scss';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
